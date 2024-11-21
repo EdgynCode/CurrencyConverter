8432144deff5753631e69d9a48e3bbe099d36385
EdgynCode <nguyenhuutan080603@gmail.com>
2024-11-21 21:44:33 +0700

    modify layout

--- app/src/main/java/com/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/currencyconverter/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.currencyconverter
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.currencyconverter.controllers.ConvertFragment
 import com.currencyconverter.controllers.LiveExchangeRatesFragment
 import com.currencyconverter.controllers.SettingsFragment
